9e3c25ef197d1b1cfa5bef6d72bab9c4221ecdee
ecpshin <francisco.shin@gmail.com>
2020-01-14 15:42:38 -0300

    Novo commit

--- src/application/ProgramConn.java
+++ src/application/ProgramConn.java
@@ -1,17 +1,39 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class ProgramConn {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
 		
-		DB.closeConnection();
+		try{
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("select * from department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);			
+			DB.closeConnection();
+		}
 
 	}
-
 }
